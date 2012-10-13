004bc7bc915b50dfccdc2f04c73e4836539f9d0a
Scott Brady <sbradymobile@gmail.com>
2012-10-12 22:36:33 -0400

    Add status bar hide and expand notifications to hardware key override
    Change-Id: I3c2b4f89c83d27e6f5a4c28c264cdfdc873b2f17

--- src/com/android/settings/cyanogenmod/HardwareKeys.java
+++ src/com/android/settings/cyanogenmod/HardwareKeys.java
@@ -51,6 +51,8 @@ public class HardwareKeys extends SettingsPreferenceFragment implements OnPrefer
     private static final int ACTION_SEARCH = 3;
     private static final int ACTION_VOICE_SEARCH = 4;
     private static final int ACTION_IN_APP_SEARCH = 5;
+    private static final int ACTION_EXPAND = 6;
+    private static final int ACTION_HIDE = 7;
 
     // Masks for checking presence of hardware keys.
     // Must match values in frameworks/base/core/res/res/values/config.xml
