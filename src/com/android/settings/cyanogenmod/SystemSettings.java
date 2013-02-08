d31845c7cc6016ed463156a8e0d78b3ace9e2483
Scott Brady <sbradymobile@gmail.com>
2013-02-08 12:21:32 -0500

    We don't have the navigation bar categary
    Change-Id: I5cc8adc68462ee579f8e63f6ed345b9694e6dc38

--- src/com/android/settings/cyanogenmod/SystemSettings.java
+++ src/com/android/settings/cyanogenmod/SystemSettings.java
@@ -93,14 +93,14 @@ public class SystemSettings extends SettingsPreferenceFragment {
             if (removeKeys) {
                 prefScreen.removePreference(findPreference(KEY_HARDWARE_KEYS));
             }
-            if (removeNavbar) {
-                prefScreen.removePreference(findPreference(KEY_NAVIGATION_BAR));
-            }
+            //if (removeNavbar) {
+            //    prefScreen.removePreference(findPreference(KEY_NAVIGATION_BAR));
+            //}
         } else {
             // Secondary user is logged in, remove all primary user specific preferences
             prefScreen.removePreference(findPreference(KEY_BATTERY_LIGHT));
             prefScreen.removePreference(findPreference(KEY_HARDWARE_KEYS));
-            prefScreen.removePreference(findPreference(KEY_NAVIGATION_BAR));
+            //prefScreen.removePreference(findPreference(KEY_NAVIGATION_BAR));
             prefScreen.removePreference(findPreference(KEY_STATUS_BAR));
             prefScreen.removePreference(findPreference(KEY_QUICK_SETTINGS));
             prefScreen.removePreference(findPreference(KEY_POWER_MENU));
