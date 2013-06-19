eb0428ba1665566c33fbdfe18f157c73ad19c6f0
Scott Brady <sbradymobile@gmail.com>
2013-06-19 10:03:23 -0400

    Restore some settings for secondary users
    Change-Id: I418780a64f52063cb8682234dc20881261274e3a

--- src/com/android/settings/cyanogenmod/SystemSettings.java
+++ src/com/android/settings/cyanogenmod/SystemSettings.java
@@ -79,11 +79,9 @@ public class SystemSettings extends SettingsPreferenceFragment implements
 
         // Only show the hardware keys config on a device that does not have a navbar
         // and the navigation bar config on phones that has a navigation bar
-        boolean removeKeys = false;
-        boolean removeNavbar = false;
         if (getResources().getInteger(
-            com.android.internal.R.integer.config_deviceHardwareKeys) == 0) {
-            removeKeys = true;
+                com.android.internal.R.integer.config_deviceHardwareKeys) == 0) {
+            prefScreen.removePreference(findPreference(KEY_HARDWARE_KEYS));
         }
 
         // Determine which user is logged in
@@ -99,22 +97,10 @@ public class SystemSettings extends SettingsPreferenceFragment implements
                     mBatteryPulse = null;
                 }
             }
-
-            // Act on the above
-            if (removeKeys) {
-                prefScreen.removePreference(findPreference(KEY_HARDWARE_KEYS));
-            }
         } else {
             // Secondary user is logged in, remove all primary user specific preferences
             prefScreen.removePreference(findPreference(KEY_BATTERY_LIGHT));
-            prefScreen.removePreference(findPreference(KEY_HARDWARE_KEYS));
-            prefScreen.removePreference(findPreference(KEY_STATUS_BAR));
-            prefScreen.removePreference(findPreference(KEY_QUICK_SETTINGS));
-            prefScreen.removePreference(findPreference(KEY_POWER_MENU));
             prefScreen.removePreference(findPreference(KEY_NOTIFICATION_DRAWER));
-            prefScreen.removePreference(findPreference(KEY_NAVIGATION_CONTROL));
-            prefScreen.removePreference(findPreference(KEY_USER_INTERFACE));
-            prefScreen.removePreference(mBarSettings);
         }
 
         // Preferences that applies to all users
@@ -153,8 +139,10 @@ public class SystemSettings extends SettingsPreferenceFragment implements
 
         mBarSettings = (PreferenceScreen) findPreference(KEY_BAR_SETTINGS);
         mQuickSettings = (PreferenceScreen) findPreference(KEY_QUICK_SETTINGS);
-        mNotificationPanel = (PreferenceScreen) findPreference(KEY_NOTIFICATION_DRAWER);
 
+        if (mIsPrimary) {
+            mNotificationPanel = (PreferenceScreen) findPreference(KEY_NOTIFICATION_DRAWER);
+        }
         boolean tabletMode = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
                 Settings.System.TABLET_MODE,
                 getActivity().getApplicationContext().getResources().getBoolean(
@@ -163,8 +151,12 @@ public class SystemSettings extends SettingsPreferenceFragment implements
         if (!tabletMode && mBarSettings != null) {
             getPreferenceScreen().removePreference(mBarSettings);
         } else if (tabletMode) {
-            if (mQuickSettings != null) getPreferenceScreen().removePreference(mQuickSettings);
-            if (mNotificationPanel != null) getPreferenceScreen().removePreference(mNotificationPanel);
+            if (mQuickSettings != null) {
+                getPreferenceScreen().removePreference(mQuickSettings);
+            }
+            if (mIsPrimary && mNotificationPanel != null) {
+                getPreferenceScreen().removePreference(mNotificationPanel);
+            }
         }
 
         // Expanded desktop
